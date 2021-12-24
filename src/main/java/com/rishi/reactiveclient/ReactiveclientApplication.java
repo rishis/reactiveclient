182fd40493f8ddedbd83b04b2a1330f6b171ad1f
Rishi Shehrawat <rishishehrawat@gmail.com>
2021-12-24 10:20:49 +0530

    Initial commit for building reactive client

---
+++ src/main/java/com/rishi/reactiveclient/ReactiveclientApplication.java
@@ -0,0 +1,13 @@
+package com.rishi.reactiveclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rishi/reactiveclient/ReactiveclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishi.reactiveclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
